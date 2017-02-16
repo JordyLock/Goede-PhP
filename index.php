56fda619c5d0b32a9ca5a0048be2e42c8fb92e65
DESKTOP-9RO9TVF\jordy <99039425@mydavinci.nl>
2017-02-16 15:22:07 +0100

    nog een paar kleine aanpassingen

--- index.php
+++ index.php
@@ -1,29 +1,59 @@
-<!DOCTYPE html>
-<html>
-<head>
-<link rel="stylesheet" type="text/css" href="stylesheet.css"></link>
-	<title>Goedemorgen</title>
-	<meta http-equiv="refresh" content="1" >
-</head>
-<body>
-
 <?php
 
 
 date_default_timezone_set("Europe/Amsterdam");
 
 
-if (date("H")<12) {
-	echo "<h1>Goede Morgen!";
+$uur = date("H");
+$minuut = date("i");
+$seconde = date("s");
+
+if ($uur >= 0)
+{
+
+		$tijd = "nacht";
+		if ($uur >= 6)
+
+		{
+			$tijd = "morgen";
+			if ($uur >= 12)
+			{
+				$tijd = "middag";
+				if ($uur >= 18)
+				{
+					$tijd = "avond";
+					
+
+				}
+
+			}
+
+		}	
+
 }
-else if (date("H")>=12){
-	echo "<h1>Goede Middag!";
 
 
-}
 
-echo   "Het is nu </h1>". date("<h1>H:i:s</h1>");
+
+
 
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="stylesheet.css"></link>
+	<title>Goedemorgen | Jordy Lock</title>
+	<meta http-equiv="refresh" content="1" >
+</head>
+<body class="<?php echo $tijd;?>">
+
+	<div class="base">
+		<h1>Goeden <?php echo $tijd; ?>!</h1>
+		<h1>het is nu  <?php echo "$uur:$minuut:$seconde"; ?></h1>
+	</div>
+
+
 </body>
 </html>
+
+
