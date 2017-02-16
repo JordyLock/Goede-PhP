13ea2683bb27b081a1ba4f46ba26d6b7ce552c7d
DESKTOP-9RO9TVF\jordy <99039425@mydavinci.nl>
2017-02-16 11:06:39 +0100

    Achtergrond met basic php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="style.css">
+	<title>Goedemorgen</title>
+</head>
+<body>
+<?php
+
+
+$color = "red";
+
+
+
+echo "My laptop is " . $color . "";
+
+
+?>
+</body>
+</html>
